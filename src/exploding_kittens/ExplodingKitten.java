d0d0acd9018ef88986b1664106b51871a1107d1a
Margherita <margherita.balzoni@studio.unibo.it>
2026-02-26 18:28:20 +0100

    add ExplodingKitten entry point

---
+++ src/exploding_kittens/ExplodingKitten.java
@@ -0,0 +1,9 @@
+package exploding_kittens;
+
+public class ExplodingKitten {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Exploding Kittens!");
+        System.out.println("This is a placeholder for the game implementation.");
+        System.out.println("Please run the unit tests to see the game logic in action.");
+    }
+}
--- src/exploding_kittens/game/model/GameBootstrap.java
+++ src/exploding_kittens/game/model/GameBootstrap.java
@@ -1,10 +1,13 @@
 package exploding_kittens.game.model;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 //inizializza il gioco e crea lo stato che gli agenti useranno
 public class GameBootstrap {
+    private static final int INITIAL_HAND_SIZE = 7;
+
     private final List<Player> players;
     private final Deck deck;
 
@@ -14,32 +17,21 @@ public class GameBootstrap {
         initializeGame();
     }
 
-    private void initializeGame(){
+    private void initializeGame() {
         populateDeck();
         deck.shuffle();
-        //da ad ogni giocatore una carta Defuse e 7 carte casuali dal mazzo
-        for (Player player : players) {
-            player.addCardToHand(new Card(CardType.DEFUSE));
-            for (int i = 0; i < INITIAL_HAND_SIZE; i++) {
-                player.addCardToHand(deck.removeTopCard());
-            }
-        //rinserisce le carte Exploding Kitten nel mazzo, in numero pari al numero di giocatori - 1
-        for (int i = 0; i < players.size() - 1; i++) {
-            deck.addCard(new Card(CardType.EXPLODING_KITTEN));
-        }
-        deck.shuffle();
     }
 
-    private void populateDeck() {
-        for (int i = 0; i < 4; i++) deck.addCard(new Card(CardType.ATTACK));
-        for (int i = 0; i < 4; i++) deck.addCard(new Card(CardType.SKIP));
-        for (int i = 0; i < 4; i++) deck.addCard(new Card(CardType.SHUFFLE));
-        for (int i = 0; i < 5; i++) deck.addCard(new Card(CardType.SEE_THE_FUTURE));
-        for (int i = 0; i < 4; i++) deck.addCard(new Card(CardType.NOPE));
-        for (int i = 0; i < 6; i++) deck.addCard(new Card(CardType.DEFUSE));
+    private void populateDeck () {
+            for (int i = 0; i < 4; i++) deck.addCard(new Card(CardType.ATTACK, "", ""));
+            for (int i = 0; i < 4; i++) deck.addCard(new Card(CardType.SKIP, "", ""));
+            for (int i = 0; i < 4; i++) deck.addCard(new Card(CardType.SHUFFLE, "", ""));
+            for (int i = 0; i < 5; i++) deck.addCard(new Card(CardType.SEE_THE_FUTURE, "", ""));
+            for (int i = 0; i < 6; i++) deck.addCard(new Card(CardType.DEFUSE, "", ""));
     }
 
-    public GameState buildGame(){
-        return new GameState(deck, players);
+    public GameState buildGame () {
+            return new GameState(deck, players);
     }
+
 }
