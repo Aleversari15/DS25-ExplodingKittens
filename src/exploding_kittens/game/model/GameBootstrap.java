39245b899793c66b121959c0e5e71625c731ea3d
Margherita <margherita.balzoni@studio.unibo.it>
2026-02-26 15:54:14 +0100

    Initialize game state in GameBootstrap class

--- src/exploding_kittens/game/model/GameBootstrap.java
+++ src/exploding_kittens/game/model/GameBootstrap.java
@@ -3,13 +3,19 @@ package exploding_kittens.game.model;
 import java.util.Collections;
 import java.util.List;
 
+//inizializza il gioco e crea lo stato che gli agenti useranno
 public class GameBootstrap {
     private final List<Player> players;
     private final Deck deck;
 
     public GameBootstrap(List<Player> players, Deck deck) {
-        this.players = players;
-        this.deck = deck;
+        this.players = new ArrayList<>(players);
+        this.deck = new Deck();
+        initializeGame();
+    }
+
+    private void initializeGame(){
+
     }
 
     public List<Player> getPlayers() {
