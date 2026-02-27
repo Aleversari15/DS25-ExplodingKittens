079095200f200fe92e62d1cdd97a901e62b30ebf
Margherita <margherita.balzoni@studio.unibo.it>
2026-02-27 11:36:33 +0100

    Add unit tests for GameState and CardFactory

---
+++ tests/CardFactoryTest.java
@@ -0,0 +1,29 @@
+package exploding_kittens.tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameStateTest {
+
+    @BeforeEach
+    void setUp() {
+        //TODO
+    }
+
+    @Test
+    void testNextPlayer() {
+        //TODO
+    }
+
+    @Test
+    void testAddPlayer() {
+        //TODO
+    }
+
+    @Test
+    void testWinner() {
+        //TODO
+    }
+}
---
+++ tests/GameStateTest.java
@@ -0,0 +1,26 @@
+package exploding_kittens.tests;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameStateTest {
+
+    @BeforeEach
+    void setUp() {
+        //TODO
+    }
+
+    @Test
+    void testDeckSize() {
+        //TODO
+    }
+
+    @Test
+    void explodingKittenCount() {
+        //TODO
+    }
+
+}
