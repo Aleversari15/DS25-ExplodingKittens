d7b7265b12cabb510855bbbae037499d6e090f36
Margherita <margherita.balzoni@studio.unibo.it>
2026-03-17 15:39:51 +0100

    Implemented GameStateTest

--- tests/CardFactoryTest.java
+++ tests/CardFactoryTest.java
@@ -1,29 +1,43 @@
 package exploding_kittens.tests;
 
+import exploding_kittens.game.model.GameState;
+import exploding_kittens.game.model.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class GameStateTest {
+class CardFactoryTest {
+
+    private GameState gameState;
+    private Player player1, player2, player3;
 
     @BeforeEach
     void setUp() {
-        //TODO
+        gameState = new GameState();
+        player1 = new Player("Agent1", "Player1");
+        player2 = new Player("Agent2", "Player2");
+        player3 = new Player("Agent3", "Player3");
+
+        gameState.addPlayer(player1);
+        gameState.addPlayer(player2);
+        gameState.addPlayer(player3);
     }
 
     @Test
-    void testNextPlayer() {
-        //TODO
+    void testInitialCurrentPlayer() {
+        assertEquals(player1, gameState.getCurrentPlayer());
     }
 
     @Test
-    void testAddPlayer() {
-        //TODO
+    void testDeckSize() {
+
     }
 
     @Test
-    void testWinner() {
+    void explodingKittenCount() {
+        setUp();
         //TODO
     }
+
 }
