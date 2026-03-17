2e995ebfb4b534eb5298e7558a0cb4c3fd21c26a
Alessandra Versari <alessandra.versari2@studio.unibo.it>
2026-03-17 13:31:32 +0100

    Added javadoc to CardFactory.java

--- src/exploding_kittens/game/model/CardFactory.java
+++ src/exploding_kittens/game/model/CardFactory.java
@@ -3,32 +3,35 @@ package exploding_kittens.game.model;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Permette di creare il mazzo di partenza, composto nel seguente modo:
+ * - Numero di Exploding Kittens pari al numero di giocatori -1
+ * - Numero di carte Defuse pari al numero di giocatori + 2
+ * - 4 carte azione per ogni tipo (skip, attack, shuffle e see the future)
+ * - 8 carte gatto (utili solo se utilizzate in coppia)
+ */
 public class CardFactory {
 
     public static List<Card> createStandardDeck(int playerCount) {
         List<Card> cards = new ArrayList<>();
 
-        // Exploding Kittens: num giocatori - 1
         for (int i = 0; i < playerCount - 1; i++)
-            cards.add(new Card(CardType.EXPLODING_KITTEN, "Exploding Kitten", "You explode unless you have a Defuse."));
+            cards.add(new Card(CardType.EXPLODING_KITTEN, "Exploding Kitten", "Esplodi! (A meno che tu non abbia una carta Defuse)"));
 
-        // Defuse: num giocatori  + qualcuno in più nel mazzo
         for (int i = 0; i < playerCount + 2; i++)
-            cards.add(new Card(CardType.DEFUSE, "Defuse", "Neutralizes an Exploding Kitten."));
+            cards.add(new Card(CardType.DEFUSE, "Defuse", "Neutralizza un Exploding Kitten"));
 
-        /*---------------CARTE AZIONE--------------------*/
         for (int i = 0; i < 4; i++)
-            cards.add(new Card(CardType.SKIP, "Skip", "End your turn without drawing."));
+            cards.add(new Card(CardType.SKIP, "Skip", "Termina il tuo turno senza pescare"));
         for (int i = 0; i < 4; i++)
-            cards.add(new Card(CardType.ATTACK, "Attack", "Force the next player to take two turns."));
+            cards.add(new Card(CardType.ATTACK, "Attack", "Forza il prossimo giocatore a giocare due turni"));
         for (int i = 0; i < 4; i++)
-            cards.add(new Card(CardType.SHUFFLE, "Shuffle", "Shuffle the draw pile."));
+            cards.add(new Card(CardType.SHUFFLE, "Shuffle", "Mischia il mazzo"));
         for (int i = 0; i < 4; i++)
-            cards.add(new Card(CardType.SEE_THE_FUTURE, "See the Future", "Peek at the top 3 cards."));
+            cards.add(new Card(CardType.SEE_THE_FUTURE, "See the Future", "Pesca e guarda le prime 3 carte del mazzo"));
 
-        // Carte Gatto: da utilizzare a coppie per applicare un effetto
         for (int i = 0; i < 8; i++)
-            cards.add(new Card(CardType.CAT_CARD, "Cat Card", "Play as pairs to steal a random card."));
+            cards.add(new Card(CardType.CAT_CARD, "Cat Card", "Se utilizzata insieme ad un'altra carta Gatto permette di pescare una carta random dalla mano di un altro giocatore"));
 
         return cards;
     }
