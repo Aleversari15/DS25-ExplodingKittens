f3b13c8365eea54347ae88a42b6206b915c584f2
Margherita <margherita.balzoni@studio.unibo.it>
2026-04-17 13:07:02 +0200

    Fix game setup and player hand initialization in GameMasterAgent

--- src/exploding_kittens/remote/GameMasterAgent.java
+++ src/exploding_kittens/remote/GameMasterAgent.java
@@ -126,7 +126,20 @@ public class GameMasterAgent extends AbstractMasterAgent {
     private class StartGameBehaviour extends OneShotBehaviour {
         @Override
         public void action() {
-            setupAndStartGame();
+            deck = DeckBuilder.prepareBaseDeck(expectedPlayers);
+            Map<String, String> hands = DeckBuilder.buildPlayerHands(deck, gameState.getActivePlayers());
+            DeckBuilder.insertExplodingKittens(deck, expectedPlayers);
+
+            for (Player player : gameState.getActivePlayers()) {
+                ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+                msg.addReceiver(new AID(player.getAgentName(), true));
+                msg.setContent(Messages.HAND_INIT + hands.get(player.getAgentName()));
+                send(msg);
+            }
+
+            System.out.println("Partita avviata!");
+            broadcastPlayersList();
+            nextTurn();
             addBehaviour(new HandleActionBehaviour());
             startPlayerTimeoutChecker();
         }
