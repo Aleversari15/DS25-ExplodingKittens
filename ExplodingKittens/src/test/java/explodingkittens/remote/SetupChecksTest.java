35392e5ff03204da3dd066346edf66412668b1bd
Margherita <margherita.balzoni@studio.unibo.it>
2026-04-24 16:27:43 +0200

    Refactor package structure for PlayerDisconnectionTest, SetupChecksTest, and TestPlayerAgent

--- ExplodingKittens/src/test/java/explodingkittens/remote/SetupChecksTest.java
+++ ExplodingKittens/src/test/java/explodingkittens/remote/SetupChecksTest.java
@@ -1,6 +1,5 @@
-package explodingkittens;
+package explodingkittens.remote;
 
-import explodingkittens.remote.Messages;
 import jade.core.Profile;
 import jade.core.ProfileImpl;
 import jade.core.Runtime;
